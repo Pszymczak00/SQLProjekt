13f54276dc30e9c3fb1cf38f0bd4f401099c2095
Pszymczak00 <przemyslaw.p.szymczak@student.put.poznan.pl>
2022-01-14 00:31:43 +0100

    Add project files.

---
+++ SQLProjekt/DBConnection.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLProjekt
+{
+    public static class DBConnection
+    {
+        public static DataTable ConnectionFun()
+        {
+            using (SqlConnection conn = new SqlConnection("Data Source=LAPTOP-48S3MQBQ;Initial Catalog=Firma;User ID=User;Password=Pass1234;Integrated Security=False;"))
+            {
+               conn.Open();
+
+                // 1.  create a command object identifying the stored procedure
+                SqlCommand cmd = new SqlCommand("[dbo].[procGetPracownicy]", conn);
+                // 2. set the command object so it knows to execute a stored procedure
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // 3. add parameter to command, which will be passed to the stored procedure
+                //cmd.Parameters.Add(new SqlParameter("@CustomerID", custId));
+
+                // execute the command
+                
+                using(var da = new SqlDataAdapter(cmd))
+                {
+                    DataTable table = new DataTable();
+                    da.Fill(table);
+                    return table;
+                }
+            }
+        }
+
+
+        public static DataTable ConnectionFun2()
+        {
+            using (SqlConnection conn = new SqlConnection("Data Source=LAPTOP-48S3MQBQ;Initial Catalog=Firma;User ID=User;Password=Pass1234;Integrated Security=False;"))
+            {
+                conn.Open();
+
+                // 1.  create a command object identifying the stored procedure
+                SqlCommand cmd = new SqlCommand("[dbo].[procGetStanowiska]", conn);
+                // 2. set the command object so it knows to execute a stored procedure
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // 3. add parameter to command, which will be passed to the stored procedure
+                //cmd.Parameters.Add(new SqlParameter("@CustomerID", custId));
+
+                // execute the command
+
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    DataTable table = new DataTable();
+                    da.Fill(table);
+                    return table;
+                }
+            }
+        }
+
+
+
+        public static DataTable ConnectionFun3()
+        {
+            using (SqlConnection conn = new SqlConnection("Data Source=LAPTOP-48S3MQBQ;Initial Catalog=Firma;User ID=User;Password=Pass1234;Integrated Security=False;"))
+            {
+                conn.Open();
+
+                // 1.  create a command object identifying the stored procedure
+                SqlCommand cmd = new SqlCommand("[dbo].[procGetZespoły]", conn);
+                // 2. set the command object so it knows to execute a stored procedure
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // 3. add parameter to command, which will be passed to the stored procedure
+                //cmd.Parameters.Add(new SqlParameter("@CustomerID", custId));
+
+                // execute the command
+
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    DataTable table = new DataTable();
+                    da.Fill(table);
+                    return table;
+                }
+            }
+        }
+    }
+}
---
+++ SQLProjekt/VievModel/TestOknoViev.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLProjekt.VievModel
+{
+    public class TestOknoViev : ViewModelBase
+    {
+        private bool buttonIsEnabled = true;
+        public bool ButtonIsEnabled
+        {
+            get { return buttonIsEnabled; }
+            set
+            {
+                if(buttonIsEnabled != value)
+                {
+                    buttonIsEnabled = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private DataTable table;
+        public DataTable Table {
+            get
+            {
+                return table;
+            }
+            set
+            {
+                table = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> Buttons { get; set; } = new ObservableCollection<string>() { "Pracownicy", "Stanowiska", "Zespoły" };
+
+        public ObservableCollection<string> Pracownik { get; set; } = new ObservableCollection<string>(); 
+
+        public TestOknoViev()
+        {
+            Table = DBConnection.ConnectionFun();
+
+            foreach (DataColumn item in Table.Columns)
+            {
+                Pracownik.Add(item.ColumnName + " : " + item.DefaultValue.ToString());
+            }
+        }
+    }
+}
